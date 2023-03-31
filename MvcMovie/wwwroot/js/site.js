1a59aec689bed8c4e4ff05567d7cd99e04e6ecc6
Jeffrey-Pai <110954961+Jeffrey-Pai@users.noreply.github.com>
2023-03-31 18:24:54 +0800

    新增select功能

--- MvcMovie/wwwroot/js/site.js
+++ MvcMovie/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Movie Batch js
+const selectAllCheckbox = document.querySelector('#select-all');
+const checkboxItems = document.querySelectorAll('.checkbox-item');
+
+selectAllCheckbox.addEventListener('change', () => {
+    for (const checkboxItem of checkboxItems) {
+        checkboxItem.checked = selectAllCheckbox.checked;
+
+
+    };
+
+});
